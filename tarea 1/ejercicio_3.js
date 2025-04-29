4e35665fa7f4f6a5b41efb61de7df88c7e0c5136
jorge diaz <jorge.diaz2001@alumnos.ubiobio.cl>
2025-04-28 22:04:31 -0400

    tarea atrasada

---
+++ tarea 1/ejercicio_3.js
@@ -0,0 +1,5 @@
+const saludo = (nombre) => {
+    return `hola, ${nombre} `
+}
+
+console.log(saludo("juan"));
---
+++ tarea 1/ejercicio_4.js
@@ -0,0 +1,8 @@
+const fruta = ["manzana", "platano", "mandarina", "frutilla", "pera"]
+
+for (let index = 0; index < fruta.length; index++) {
+    const element = fruta[index];
+    console.log(fruta[index]);
+    
+    
+}
