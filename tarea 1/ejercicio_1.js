5125d3dee0bf8305796ed8775d70bd0b392dc856
jorge diaz <jorge.diaz2001@alumnos.ubiobio.cl>
2025-04-28 21:57:55 -0400

    tarea atrasada

---
+++ tarea 1/ejercicio_1.js
@@ -0,0 +1,7 @@
+const nombre = "jose"
+const edad = "35"
+const mascota = true
+
+console.log(nombre);
+console.log(edad);
+console.log(mascota);
---
+++ tarea 1/ejercicio_2.js
@@ -0,0 +1,8 @@
+const numero1 = "25"
+const numeto2 = "5"
+
+console.log(`suma: ${numero1} + ${numeto2} = ${numero1 + numeto2}`);
+console.log(`resta: ${numero1} - ${numeto2} = ${numero1 - numeto2}`);
+console.log(`multiplicacion: ${numero1} * ${numeto2} = ${numero1 * numeto2}`);
+console.log(`division: ${numero1} / ${numeto2} = ${numero1 / numeto2}`);
+ 
